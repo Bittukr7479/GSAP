bafced4a94db767634bb948201b53be40cf31377
Bittu <123286618+Bittukr7479@users.noreply.github.com>
2024-05-18 16:03:40 +0530

    GSAP Assignment

---
+++ script.js
@@ -0,0 +1,18 @@
+
+gsap.to("#box1",{
+    x:1200,
+    duration:2,
+    rotate:360,
+    delay:1,
+    backgroundColor:"blue",
+    borderRadius:"50%"
+})
+
+gsap.from("#box2",{
+    x:1200,
+    duration:1,
+    rotate:360,
+    delay:1,
+    backgroundColor:"red",
+    borderRadius:"50%"
+})
